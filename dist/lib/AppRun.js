196a281d1fd2c2b09ac860184753a40dbb863e58
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-01-02 15:00:39 +0200

    1.2.1 - added some paths exports in AppRun script

--- dist/lib/AppRun.js
+++ dist/lib/AppRun.js
@@ -3,8 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class AppRun {
     static generate(binary) {
         return [
-            '#!/bin/bash', '',
-            'cd "$APPDIR"', '',
+            '#!/bin/bash',
+            '',
+            'if [ -z "$APPDIR" ] ; then',
+            '   path="$(dirname "$(readlink -f "${THIS}")")"',
+            '   while [[ "$path" != "" && ! -e "$path/$1" ]]; do',
+            '       path=${path%/*}',
+            '   done',
+            '   APPDIR="$path"',
+            'fi',
+            '',
+            'export PATH="${APPDIR}:${APPDIR}/usr/sbin:${PATH}"',
+            'export XDG_DATA_DIRS="./share/:/usr/share/gnome:/usr/local/share/:/usr/share/:${XDG_DATA_DIRS}"',
+            'export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"',
+            'export XDG_DATA_DIRS="${APPDIR}"/usr/share/:"${XDG_DATA_DIRS}":/usr/share/gnome/:/usr/local/share/:/usr/share/',
+            'export GSETTINGS_SCHEMA_DIR="${APPDIR}/usr/share/glib-2.0/schemas:${GSETTINGS_SCHEMA_DIR}"',
+            '',
+            'cd "$APPDIR"',
+            '',
             `exec "$APPDIR/${binary}"`
         ].join('\n');
     }
